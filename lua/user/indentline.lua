01e7f01e3aece8474def9a5e6e68e86d6d14c214
v4rgas <juanvargas@uc.cl>
2022-06-30 21:18:40 -0400

    changes of tempalte

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -5,13 +5,12 @@ end
 
 indent_blankline.setup {
   show_current_context = true,
-  indent_blankline_char = "▏",
-  indent_blankline_show_trailing_blankline_indent = false,
-  indent_blankline_show_first_indent_level = true,
-  indent_blankline_use_treesitter = true,
-  indent_blankline_show_current_context = true,
-  indent_blankline_buftype_exclude = { "terminal", "nofile" },
-  indent_blankline_filetype_exclude = {
+  char = "▏",
+  show_trailing_blankline_indent = false,
+  show_first_indent_level = true,
+  use_treesitter = true,
+  buftype_exclude = { "terminal", "nofile" },
+  filetype_exclude = {
     "help",
     "packer",
     "NvimTree",
--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -3,16 +3,16 @@ if not null_ls_status_ok then
 	return
 end
 
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+local completion = null_ls.builtins.completion
 
--- https://github.com/prettier-solidity/prettier-plugin-solidity
--- npm install --save-dev prettier prettier-plugin-solidity
 null_ls.setup({
 	debug = false,
 	sources = {
+    -- general spell
+    -- completion.codespell,
+
 		-- css
 		formatting.prettier.with({
 			extra_filetypes = { "css" },
